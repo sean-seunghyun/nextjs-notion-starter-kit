da8e047a3a249820b13d16618b28627eaf469c55
Seunghyun Kim <seankim@mediquitous.com>
2024-01-24 15:26:12 +0900

    favicon 수정

--- site.config.ts
+++ site.config.ts
@@ -9,9 +9,9 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '알게된 것들을 기록합니다.',
+  domain: 'https://dont-stop-it-sean.vercel.app/',
+  author: 'Sean Kim',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
