92adec944f99c1c20f4adf1f8627a533cdfdf0e9
Seunghyun Kim <seankim@mediquitous.com>
2024-01-24 14:20:59 +0900

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '4d0193349ed049dbbebf563f619d7b79',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
